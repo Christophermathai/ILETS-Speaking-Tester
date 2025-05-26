0a9d115f4127695fc2c18e64c1ba615e241f695d
Christophermathai <christophermathai123@gmail.com>
2025-05-26 15:00:01 +0530

    Changes made for database setup

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,19 +1,37 @@
 import { MongoClient, MongoClientOptions } from "mongodb";
 
-const uri = "mongodb://admin:123@localhost:27017/?authSource=admin";
+const uri = process.env.MONGODB_URI;
 const options: MongoClientOptions = {};
 
+if (!uri) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+// Add type to the global object
 declare global {
+  // allow global variable in Node.js
   // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-let client: MongoClient;
-
-if (!global._mongoClientPromise) {
+if (process.env.NODE_ENV === "development") {
+  // In development, reuse the global variable to prevent multiple connections
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production, no need to use global
   client = new MongoClient(uri, options);
-  global._mongoClientPromise = client.connect();
+  clientPromise = client.connect();
 }
-const clientPromise = global._mongoClientPromise!;
 
 export default clientPromise;
+
+
+
+
